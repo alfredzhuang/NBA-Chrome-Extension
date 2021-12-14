4359204f6437be61b5238059e90002d11b3cd996
alfredzhuang <alfredzhuang123@gmail.com>
2021-12-13 21:26:29 -0800

    add the info about the games that are scheduled at the current date

--- background.js
+++ background.js
@@ -4,10 +4,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
-    console.log(month);
 
     const formattedDate = `${year}${month}${day}`;
-    console.log(formattedDate);
 
     fetch(
       "http://data.nba.net/10s/prod/v1/" + formattedDate + "/scoreboard.json"
--- scripts/home-script.js
+++ scripts/home-script.js
@@ -1,3 +1,38 @@
 chrome.runtime.sendMessage({ message: "getScoreboard" }, function (response) {
   let data = response;
+
+  //parse the data
+  let games = data.games;
+
+  // Create html elements for each game
+  games.forEach(function (game) {
+    // create the div
+    const div = document.createElement("div");
+    div.className = "game";
+
+    // add text content
+    const location = document.createElement("p");
+    location.textContent = `${game.arena.name} | ${game.arena.city} | ${game.arena.stateAbbr} | ${game.arena.country}`;
+    div.appendChild(location);
+
+    const time = document.createElement("p");
+    time.textContent = `Start time: ${game.startTimeEastern} | Current Duration: ${game.gameDuration.hours} hours and ${game.gameDuration.minutes} minutes`;
+    div.appendChild(time);
+
+    // game
+    const gameStats = document.createElement("div");
+
+    teams = document.createElement("p");
+    teams.textContent = `${game.vTeam.triCode} (visitors) vs. ${game.hTeam.triCode} (home)`;
+    gameStats.append(teams);
+
+    score = document.createElement("p");
+    score.textContent = `${game.vTeam.score} - ${game.hTeam.score}`;
+    gameStats.append(score);
+
+    div.appendChild(gameStats);
+
+    // append to the html page
+    document.querySelector("#games").appendChild(div);
+  });
 });
