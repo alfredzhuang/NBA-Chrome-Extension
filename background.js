ae58ebe9f99c2110bb0550b6d34e9f8b91258bf8
alfredzhuang <alfredzhuang123@gmail.com>
2021-12-13 21:05:56 -0800

    fix the month number for api call

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "getScoreboard") {
     const date = new Date();
-    const month = date.getMonth();
+    const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
+    console.log(month);
 
     const formattedDate = `${year}${month}${day}`;
+    console.log(formattedDate);
 
     fetch(
       "http://data.nba.net/10s/prod/v1/" + formattedDate + "/scoreboard.json"
